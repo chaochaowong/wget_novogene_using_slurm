5f9173b1b92ff6719053375fffb1519f2fbbb104
Wong <cwo11@login-1.cm.cluster>
2024-09-24 16:08:46 -0700

    first commit

---
+++ wget_novogene.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=16G
+#SBATCH --time=12:00:00 
+#SBATCh --job-name=wget_novogene
+#SBATCH --partition=cpu-core-sponsored
+#SBATCH --mail-type=END 
+#SBATCH --mail-user=Chao-Jen.Wong@seattlechildrens.org 
+
+echo "FASTQ file: $1"
+echo "Destination: $2"
+fq=$1
+dest=$2
+cd $dest
+
+wget -r -c --cut-dirs=2 $fq
